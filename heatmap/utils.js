801c23136a2e96bf8a5bbe221033edd174d23ba8
Joonas Loppi <joonas@joonas.fi>
2014-10-15 21:28:47 +0300

    Refactor some noise away from bus-lines.html

---
+++ heatmap/utils.js
@@ -0,0 +1,39 @@
+var calculateCenter = function (pointsArray) {
+	var center = pointsArray.reduce(function (prev, current){
+		prev[0] += current[0];
+		prev[1] += current[1];
+
+		return prev;
+	}, [0.0, 0.0]);
+
+	center[0] /= pointsArray.length;
+	center[1] /= pointsArray.length;
+
+	return center;
+}
+
+// NOTE! Google Maps takes coordinates in format (latitude, longitude), but GeoJSON takes (longitude, latitude)
+// that's why we have to reverse the order of coordinates before putting those in GeoJSON
+var flipCoordinatesForStupidGeoJSON = function (listOfCoordinates) {
+	return listOfCoordinates.map(function (coord) {
+		return [coord[1], coord[0]];
+	});
+};
+
+// some providers that leaflet supports. you can try to change the provider to get a different looking map
+var providers = {
+	mapbox: {
+		template: 'http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png',
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
+	},
+
+	osm: {
+		template: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+	},
+
+	mapquest: {
+		template: 'http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+		attribution: 'fixme'
+	}
+};
