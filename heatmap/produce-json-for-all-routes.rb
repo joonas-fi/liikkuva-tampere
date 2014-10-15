3cbac3a5cf681ef479f7716ea7dc719c8a674f0c
Joonas Loppi <joonas@joonas.fi>
2014-10-15 22:23:00 +0300

    Forgot to commit produce-json-for-all-routes
    + remove now-unneeded produce-json-for-line and its points.js

---
+++ heatmap/produce-json-for-all-routes.rb
@@ -0,0 +1,16 @@
+require 'json'
+require './common.rb'
+
+routes = parse_gtfs_file("routes.txt")
+
+filename = "routes.js"
+
+File.open(filename, "w") do |file|
+	# the file is not strictly JSON (hence extension .js)
+	file.write "var routes = "
+
+	# the rest are JSON
+	file.write(routes.to_json)
+end
+
+puts "#{routes.length} routes(s) written to #{filename}"
