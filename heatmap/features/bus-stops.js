e3d531ad74e994f334dc2136f932535de668df15
Joonas Loppi <joonas@joonas.fi>
2014-10-16 20:29:45 +0300

    Compute map maxBounds from bus stops

--- heatmap/features/bus-stops.js
+++ heatmap/features/bus-stops.js
@@ -1,12 +1,16 @@
 
 $(document).ready(function (){
-	// create bus stop layer group (so we can treat all bus stops as one layer)
-	var busStopLayer = L.layerGroup();
+	// create bus stop feature group (so we can treat all bus stops as one layer)
+	// note: the difference of featureGroup vs layerGroup is that it contains getBounds() and supports some events etc.
+	var busStopLayer = L.featureGroup();
 
 	for(var i = 0; i < busStops.length; i++) {
 		var coords = [ busStops[i][0], busStops[i][1] ];
 		L.circle(coords, 50, { color: '#0000ff', weight: 3 }).addTo(busStopLayer);
 	}
+
+	// set map max bounds according to bus stop bounds (so user can't linger too far away from what's important)
+	map.setMaxBounds(busStopLayer.getBounds());
 
 	// when the checkbox is clicked, show or hide the bus stop layer layergroup
 	$('#showBusStops').change(function (){
